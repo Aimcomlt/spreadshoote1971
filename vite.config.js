804b2604e5ac9f927f863a839dc4c67b0593081b
Aime Comeault <67168818+Aimcomlt@users.noreply.github.com>
2025-08-03 01:42:17 -0600

    optimized-003

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/spreadshoote1971/', // ← this is crucial!
+})
