169198e82626da9fac7bcf57385aa4590f30bbb8
Aime Comeault <robinhoodblock71@gmail.com>
2025-08-02 20:29:52 -0600

    feat: respond to pointer movement

--- src/hooks/usePointerControls.js
+++ src/hooks/usePointerControls.js
@@ -8,9 +8,25 @@ export function usePointerControls(callback) {
       callback({ type: 'down', x, y, pointerType });
     };
 
+    const handlePointerMove = (event) => {
+      event.preventDefault();
+      const { clientX: x, clientY: y, pointerType } = event;
+      callback({ type: 'move', x, y, pointerType });
+    };
+
+    const handlePointerUp = (event) => {
+      event.preventDefault();
+      const { clientX: x, clientY: y, pointerType } = event;
+      callback({ type: 'up', x, y, pointerType });
+    };
+
     window.addEventListener('pointerdown', handlePointerDown);
+    window.addEventListener('pointermove', handlePointerMove);
+    window.addEventListener('pointerup', handlePointerUp);
     return () => {
       window.removeEventListener('pointerdown', handlePointerDown);
+      window.removeEventListener('pointermove', handlePointerMove);
+      window.removeEventListener('pointerup', handlePointerUp);
     };
   }, [callback]);
 }
